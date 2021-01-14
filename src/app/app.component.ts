f35d4fbf3bf06bdc06c5f8f84461d2b0e9fcf73b
Your Name <you@example.com>
2021-01-14 19:18:02 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { from } from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo';
+  dataTable = [];
+ constructor(private http:HttpClient){}
+  
+  submitdata(val: {name: string; age: number; gender: string; address: string}) {
+   this.http.post('https://ng-demo-69b0b-default-rtdb.firebaseio.com/posts.json',val).subscribe(res=> {
+   });
+  }
+
+  getData(){
+    this.http.get('https://ng-demo-69b0b-default-rtdb.firebaseio.com/posts.json')
+    .pipe(map(data=> {
+      const dataArray = [];
+      for( const item in data) {
+        dataArray.push({...data[item]});
+      }
+      return dataArray;
+    })
+    ).subscribe(res=>{
+      console.log(res);
+      this.dataTable = res;
+      console.log('data array',this.dataTable);
+    });
+  }
 }
