11ce34705e5438e2858b5b2c35dd8abd9ff0162a
Your Name <you@example.com>
2021-01-16 13:08:07 +0530

    async pipe added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { from } from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Observable }from 'rxjs';
+import { Detail } from './detail.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,26 +12,45 @@ import { map } from 'rxjs/operators';
 export class AppComponent {
   title = 'demo';
   dataTable = [];
+  data$ : Observable<Detail[]>;
+
  constructor(private http:HttpClient){}
   
-  submitdata(val: {name: string; age: number; gender: string; address: string}) {
+  submitdata(val: Detail) {
    this.http.post('https://ng-demo-69b0b-default-rtdb.firebaseio.com/posts.json',val).subscribe(res=> {
    });
   }
+//  to get the table data using subscribe
 
-  getData(){
-    this.http.get('https://ng-demo-69b0b-default-rtdb.firebaseio.com/posts.json')
+  // getData(){
+  //   this.http.get<{[key: string]: Detail}>('https://ng-demo-69b0b-default-rtdb.firebaseio.com/posts.json')
+  //   .pipe(map(data=> {
+  //     const dataArray: Detail[] = [];
+  //     console.log('type of data', typeof(data));
+  //     for( const item in data) {
+  //       dataArray.push({...data[item]});
+  //     }
+  //     return dataArray;
+  //   })
+  //   ).subscribe(res=>{
+  //     console.log(res);
+  //     this.dataTable = res;
+  //     // console.log('data array',this.dataTable);
+  //   });
+  // }
+
+// to get table data using async pipe to subscribe 
+
+   getAsyncData (){
+    this.data$ = this.http.get<{[key:string]: Detail}>('https://ng-demo-69b0b-default-rtdb.firebaseio.com/posts.json')
     .pipe(map(data=> {
-      const dataArray = [];
+      const dataArray: Detail[] = [];
+      console.log('type of data', typeof(data));
       for( const item in data) {
         dataArray.push({...data[item]});
       }
       return dataArray;
     })
-    ).subscribe(res=>{
-      console.log(res);
-      this.dataTable = res;
-      console.log('data array',this.dataTable);
-    });
-  }
-}
+    );
+   }
+ }
---
+++ src/app/detail.model.ts
@@ -0,0 +1,6 @@
+export interface Detail{
+  name: string;
+  age: number;
+  gender: string;
+  address: string;
+};
